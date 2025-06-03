8e29ce104744350c154e07fb3bcdec5e77134ed2
Alex-Yu21 <yualex2112@gmail.com>
2025-06-03 17:37:00 +0400

    feat: add CurrentLocation via geolocator

--- lib/presentation/map_page.dart
+++ lib/presentation/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 import '../../data/map_repository_impl.dart';
@@ -16,15 +17,54 @@ class _MapPageState extends State<MapPage> {
     target: Point(latitude: 59.9343, longitude: 30.3351),
     zoom: 12,
   );
+  late String lat;
+  late String long;
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location service are disabled');
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions',
+      );
+    }
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: YandexMap(
-        zoomGesturesEnabled: true,
-        onMapCreated: (controller) async {
-          await _repo.moveCamera(controller, _initialPosition);
-        },
+      body: Stack(
+        children: [
+          YandexMap(
+            zoomGesturesEnabled: true,
+            onMapCreated: (controller) async {
+              await _repo.moveCamera(controller, _initialPosition);
+            },
+          ),
+          Positioned(
+            bottom: 30,
+            right: 0,
+            child: IconButton(
+              onPressed: () {
+                _getCurrentLocation().then((value) {
+                  lat = '${value.latitude}';
+                  long = '${value.longitude}';
+                });
+              },
+              icon: Icon(Icons.my_location),
+            ),
+          ),
+        ],
       ),
     );
   }
