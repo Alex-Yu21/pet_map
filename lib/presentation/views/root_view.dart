094d143519b6dd95812af12b7b849956a2bcfaeb
Alex-Yu21 <yualex2112@gmail.com>
2025-06-08 14:52:13 +0400

    refactor(navigation): replace PageView with IndexedStack + nested navigators

--- lib/presentation/views/root_view.dart
+++ lib/presentation/views/root_view.dart
@@ -6,55 +6,78 @@ import 'package:pet_map/presentation/resources/app_dimansions.dart';
 import 'package:pet_map/presentation/views/map_view/map_view.dart';
 import 'package:pet_map/presentation/views/pets_view/pets_view.dart';
 
-class RootView extends ConsumerWidget {
+class RootView extends ConsumerStatefulWidget {
   const RootView({super.key});
+  @override
+  ConsumerState<RootView> createState() => _RootViewState();
+}
+
+class _RootViewState extends ConsumerState<RootView> {
+  final _navKeys = [GlobalKey<NavigatorState>(), GlobalKey<NavigatorState>()];
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final index = ref.watch(navIndexProvider);
-    final ctrl = ref.watch(pageCtrlProvider);
-
-    ref.listen<int>(navIndexProvider, (_, next) {
-      ctrl.jumpToPage(next);
-    });
 
     return Container(
       color: Colors.white,
       child: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
-          body: PageView(
-            controller: ctrl,
-            physics: const NeverScrollableScrollPhysics(),
-            children: const [MapView(), PetsView()],
+          body: IndexedStack(
+            index: index,
+            children: [
+              Navigator(
+                key: _navKeys[0],
+                onGenerateRoute:
+                    (settings) =>
+                        MaterialPageRoute(builder: (_) => const MapView()),
+              ),
+              Navigator(
+                key: _navKeys[1],
+                onGenerateRoute:
+                    (settings) =>
+                        MaterialPageRoute(builder: (_) => const PetsView()),
+              ),
+            ],
           ),
-          bottomNavigationBar: NavigationBarTheme(
-            data: NavigationBarThemeData(backgroundColor: Color(0xFFF3F3F3)),
-            child: NavigationBar(
-              selectedIndex: index,
-              onDestinationSelected:
-                  (i) => ref.read(navIndexProvider.notifier).state = i,
-              destinations: [
-                NavigationDestination(
-                  icon: Icon(Icons.star_border, size: IconSizes.l),
-                  label: 'Карта',
-                  selectedIcon: SvgPicture.asset(
-                    'assets/icons/star_pressed.svg',
-                    width: IconSizes.l,
-                  ),
-                ),
-                NavigationDestination(
-                  icon: Icon(Icons.star_border, size: IconSizes.l),
-                  label: 'Питомцы',
-                  selectedIcon: SvgPicture.asset(
-                    'assets/icons/star_pressed.svg',
-                    width: IconSizes.l,
-                  ),
-                ),
-              ],
+          bottomNavigationBar: _BottomBar(index: index),
+        ),
+      ),
+    );
+  }
+}
+
+class _BottomBar extends ConsumerWidget {
+  final int index;
+  const _BottomBar({required this.index});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return NavigationBarTheme(
+      data: NavigationBarThemeData(backgroundColor: Color(0xFFF3F3F3)),
+      child: NavigationBar(
+        selectedIndex: index,
+        onDestinationSelected:
+            (i) => ref.read(navIndexProvider.notifier).state = i,
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.star_border, size: IconSizes.l),
+            label: 'Карта',
+            selectedIcon: SvgPicture.asset(
+              'assets/icons/star_pressed.svg',
+              width: IconSizes.l,
             ),
           ),
-        ),
+          NavigationDestination(
+            icon: Icon(Icons.star_border, size: IconSizes.l),
+            label: 'Питомцы',
+            selectedIcon: SvgPicture.asset(
+              'assets/icons/star_pressed.svg',
+              width: IconSizes.l,
+            ),
+          ),
+        ],
       ),
     );
   }
