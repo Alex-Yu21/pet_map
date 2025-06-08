0a3b785d5eb87234e0bb52b65b9914d057faea02
Alex-Yu21 <yualex2112@gmail.com>
2025-06-08 13:46:32 +0400

    feat: add custom icons and bottom navigation bar styling

--- lib/presentation/views/root_view.dart
+++ lib/presentation/views/root_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:pet_map/presentation/providers/page_controller_provider.dart';
-import 'package:pet_map/presentation/views/map_view.dart';
+import 'package:pet_map/presentation/resources/app_dimansions.dart';
+import 'package:pet_map/presentation/views/map_view/map_view.dart';
 
 class RootView extends ConsumerWidget {
   const RootView({super.key});
@@ -15,23 +17,42 @@ class RootView extends ConsumerWidget {
       ctrl.jumpToPage(next);
     });
 
-    return Scaffold(
-      body: PageView(
-        controller: ctrl,
-        physics: const NeverScrollableScrollPhysics(),
-        children: const [MapView(), PetsView()],
-      ),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: index,
-        onDestinationSelected:
-            (i) => ref.read(navIndexProvider.notifier).state = i,
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.star_border), label: 'Карта'),
-          NavigationDestination(
-            icon: Icon(Icons.star_rounded),
-            label: 'Питомцы',
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          body: PageView(
+            controller: ctrl,
+            physics: const NeverScrollableScrollPhysics(),
+            children: const [MapView(), PetsView()],
           ),
-        ],
+          bottomNavigationBar: NavigationBarTheme(
+            data: NavigationBarThemeData(backgroundColor: Color(0xFFF3F3F3)),
+            child: NavigationBar(
+              selectedIndex: index,
+              onDestinationSelected:
+                  (i) => ref.read(navIndexProvider.notifier).state = i,
+              destinations: [
+                NavigationDestination(
+                  icon: Icon(Icons.star_border, size: IconSizes.l),
+                  label: 'Карта',
+                  selectedIcon: SvgPicture.asset(
+                    'assets/icons/star_pressed.svg',
+                    width: IconSizes.l,
+                  ),
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.star_border, size: IconSizes.l),
+                  label: 'Питомцы',
+                  selectedIcon: SvgPicture.asset(
+                    'assets/icons/star_pressed.svg',
+                    width: IconSizes.l,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
