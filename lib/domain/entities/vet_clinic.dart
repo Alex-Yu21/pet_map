3493873f14703c555cde11027d83e34572b73afd
Alex-Yu21 <yualex2112@gmail.com>
2025-06-15 20:04:24 +0400

    feat(domain): add VetClinic entities

---
+++ lib/domain/entities/vet_clinic.dart
@@ -0,0 +1,31 @@
+import 'package:yandex_mapkit/yandex_mapkit.dart';
+
+class VetClinic {
+  final String id;
+  final String name;
+  final Point point;
+  final String? address;
+  final String? phone;
+  final bool visible;
+  final bool isCustom;
+
+  const VetClinic({
+    required this.id,
+    required this.name,
+    required this.point,
+    this.address,
+    this.phone,
+    this.visible = true,
+    this.isCustom = false,
+  });
+
+  VetClinic copyWith({bool? visible}) => VetClinic(
+    id: id,
+    name: name,
+    point: point,
+    address: address,
+    phone: phone,
+    visible: visible ?? this.visible,
+    isCustom: isCustom,
+  );
+}
