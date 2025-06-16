1b01ad8a40d55199f7e9b8bbd1e9fa4a263df170
Alex-Yu21 <yualex2112@gmail.com>
2025-06-16 22:01:41 +0400

    fix: remove remote clinixs from yandexMapkit

--- lib/data/repositories/clinics_repository_impl.dart
+++ lib/data/repositories/clinics_repository_impl.dart
@@ -1,15 +1,14 @@
 import 'dart:async';
 
-import 'package:pet_map/data/datasources/local_clinic_ds.dart';
-import 'package:pet_map/data/datasources/remote_clinic_ds.dart';
-import 'package:pet_map/domain/entities/vet_clinic.dart';
-import 'package:pet_map/domain/revositories/clinics_repository.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
-class ClinicsRepositoryImpl implements ClinicsRepository {
-  ClinicsRepositoryImpl(this._remote, this._local);
+import '../../domain/entities/vet_clinic.dart';
+import '../../domain/revositories/clinics_repository.dart';
+import '../datasources/local_clinic_ds.dart';
 
-  final RemoteClinicDS _remote;
+class ClinicsRepositoryImpl implements ClinicsRepository {
+  ClinicsRepositoryImpl(this._local);
+
   final LocalClinicDS _local;
 
   final _stream = StreamController<List<VetClinic>>.broadcast();
@@ -21,16 +20,13 @@ class ClinicsRepositoryImpl implements ClinicsRepository {
   @override
   Future<List<VetClinic>> getNearby(
     Point center, {
-    double radiusMeters = 5000,
+    double radiusMeters = 5_000,
   }) async {
-    final remote = await _remote.search(center, radiusMeters);
+    final all = _local.customClinics();
     final hidden = _local.hiddenIds();
-    final custom = _local.customClinics();
 
-    _cache = [
-      ...remote.map((c) => c.copyWith(visible: !hidden.contains(c.id))),
-      ...custom,
-    ];
+    _cache =
+        all.map((c) => c.copyWith(visible: !hidden.contains(c.id))).toList();
     _stream.add(_cache);
     return _cache;
   }
--- lib/presentation/providers/clinic_providers.dart
+++ lib/presentation/providers/clinic_providers.dart
@@ -1,27 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pet_map/domain/usecases/clinics/add_clinic.dart';
-import 'package:pet_map/domain/usecases/clinics/get_nearby_clinics.dart';
-import 'package:pet_map/domain/usecases/clinics/visibility.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 import '../../data/datasources/local_clinic_ds.dart';
-import '../../data/datasources/remote_clinic_ds.dart';
 import '../../data/repositories/clinics_repository_impl.dart';
 import '../../domain/entities/vet_clinic.dart';
+import '../../domain/usecases/clinics/add_clinic.dart';
+import '../../domain/usecases/clinics/get_nearby_clinics.dart';
+import '../../domain/usecases/clinics/visibility.dart';
 
 final _localDSProvider = Provider<LocalClinicDS>((ref) {
-  final ds = LocalClinicDS();
-  ds.init();
+  final ds = LocalClinicDS()..init();
   return ds;
 });
 
-final _remoteDSProvider = Provider((_) => RemoteClinicDS());
-
 final _repoProvider = Provider<ClinicsRepositoryImpl>(
-  (ref) => ClinicsRepositoryImpl(
-    ref.read(_remoteDSProvider),
-    ref.read(_localDSProvider),
-  ),
+  (ref) => ClinicsRepositoryImpl(ref.read(_localDSProvider)),
 );
 
 final clinicsStreamProvider = StreamProvider<List<VetClinic>>(
@@ -34,7 +27,10 @@ final nearbyClinicsProvider = FutureProvider.autoDispose
       return uc(point);
     });
 
-final addClinicProvider = Provider((ref) => AddClinic(ref.read(_repoProvider)));
-final toggleClinicProvider = Provider(
+final addClinicProvider = Provider<AddClinic>(
+  (ref) => AddClinic(ref.read(_repoProvider)),
+);
+
+final toggleClinicProvider = Provider<ToggleVisibility>(
   (ref) => ToggleVisibility(ref.read(_repoProvider)),
 );
