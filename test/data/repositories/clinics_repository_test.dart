dbd296b5daefe252212c5200b7cdaeefa1129d9e
Alex-Yu21 <yualex2112@gmail.com>
2025-06-17 22:05:01 +0400

    test: update clinic repository test for local data source

--- test/data/repositories/clinics_repository_test.dart
+++ test/data/repositories/clinics_repository_test.dart
@@ -1,42 +1,36 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pet_map/data/datasources/local_clinic_ds.dart';
-import 'package:pet_map/data/datasources/remote_clinic_ds.dart';
 import 'package:pet_map/data/repositories/clinics_repository_impl.dart';
 import 'package:pet_map/domain/entities/vet_clinic.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
-class FakeRemoteClinicDS extends RemoteClinicDS {
-  @override
-  Future<List<VetClinic>> search(Point _, double __) async => [
-    VetClinic(
-      id: '1',
-      name: 'Test Vet',
-      point: const Point(latitude: 59.9343, longitude: 30.3351),
-    ),
-  ];
-}
-
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.setMockInitialValues({});
-  test('repository emits clinics from remote', () async {
-    TestWidgetsFlutterBinding.ensureInitialized();
-    SharedPreferences.setMockInitialValues({});
 
+  test('shoud emits clinics from local storage', () async {
     final local = LocalClinicDS();
     await local.init();
 
-    final repo = ClinicsRepositoryImpl(FakeRemoteClinicDS(), local);
+    final clinic = VetClinic(
+      id: '1',
+      name: 'Test Vet',
+      point: const Point(latitude: 59.9343, longitude: 30.3351),
+    );
+    await local.addCustom(clinic);
 
-    final first = repo.watchClinics().first;
+    final repo = ClinicsRepositoryImpl(local);
+
+    final firstEmission = repo.watchClinics().first;
 
     await repo.getNearby(
       const Point(latitude: 59.9, longitude: 30.3),
       radiusMeters: 5000,
     );
 
-    final list = await first;
-    expect(list.length, 1);
+    final clinics = await firstEmission;
+    expect(clinics.length, 1);
+    expect(clinics.first.id, '1');
   });
 }
